8887681cd82e7e0b6eb48b00873496e068676f61
qinrenbo <qinrenbo@sensetime.com>
2015-09-09 15:34:02 +0800

    queue buffer within max capacity

--- blocking_queue.cpp
+++ blocking_queue.cpp
@@ -1,33 +1,61 @@
 #include "blocking_queue.h"
-#include<stdio.h>
-#include<stdlib.h>
-
-namespace blocking {
 	
-	template<typename T>
-	void BlockingQueue<T>::put(const T& task){
-		std::lock_guard<std::mutex> lock(_mutex);
-		_queue.push_back(task);
-		std::cout << "put: " << task << std::endl;
-		_condvar.notify_all(); 
-	}
-
-	template<typename T>
-	T BlockingQueue<T>::take(){
-		std::unique_lock<std::mutex> lock(_mutex);
-		_condvar.wait(lock, [this]{return !_queue.empty();} );
-		assert(!_queue.empty());
-		T front(_queue.front());
-		_queue.pop_front();
-		std::cout << "take: " << front << std::endl;
-		return front;
-	}
-
-	template<typename T>
-	size_t BlockingQueue<T>::size(){
-		std::lock_guard<std::mutex> lock(_mutex);
-		return _queue.size();
-	}
-
+template<typename T>
+void BlockingQueue<T>::put(const T& task){
+	std::unique_lock<std::mutex> lock(mtx);
+	m_full.wait(lock, [this]{return (m_queue.size() < m_capacity);} );
+	assert(m_queue.size() < m_capacity);
+	m_queue.push(task);
+	m_empty.notify_all(); 
 }
 
+template<typename T>
+T BlockingQueue<T>::take(){
+	std::unique_lock<std::mutex> lock(mtx);
+	m_empty.wait(lock, [this]{return !m_queue.empty();} );
+	assert(!m_queue.empty());
+	T front(m_queue.front());
+	m_queue.pop();
+	m_full.notify_all();
+	return front;
+}
+
+template<typename T>
+size_t BlockingQueue<T>::size(){
+	std::lock_guard<std::mutex> lock(mtx);
+	return m_queue.size();
+}
+
+template<typename T>
+T BlockingQueue<T>::front(){
+	std::unique_lock<std::mutex> lock(mtx);
+	m_empty.wait(lock, [this]{return !m_queue.empty();} );
+	assert(!m_queue.empty());
+	T front(m_queue.front());
+	return front;
+}
+
+template<typename T>
+T BlockingQueue<T>::back(){
+	std::unique_lock<std::mutex> lock(mtx);
+	m_empty.wait(lock, [this]{return !m_queue.empty();} );
+	assert(!m_queue.empty());
+	T back(m_queue.back());
+	return back;
+}
+
+template<typename T>
+bool BlockingQueue<T>::empty(){
+	std::unique_lock<std::mutex> lock(mtx);
+	return m_queue.empty();
+}
+
+template<typename T>
+void BlockingQueue<T>::setCapacity(const size_t capacity){
+	m_capacity = (capacity > 0 ? capacity : MAX_CAPACITY);
+}
+	
+template class BlockingQueue<int>;
+template class BlockingQueue<float>;
+template class BlockingQueue<double>;
+template class BlockingQueue<cv::Mat>;
--- blocking_queue.h
+++ blocking_queue.h
@@ -2,34 +2,36 @@
 #define BLOCKING_QUEUE_H
 
 #include <iostream>
-#include <list>
+#include <vector>
+#include <queue>
 #include <condition_variable>
 #include <assert.h>
 
-namespace blocking {
+#include "opencv2/core/core.hpp"
 
-	template<typename T>
-	class BlockingQueue{
+#define MAX_CAPACITY 20
+template<typename T>
+class BlockingQueue{
 
-		public:
-			BlockingQueue() :_mutex(), _condvar(), _queue() { }
-			void put(const T& task);
-			T take();
-			size_t size();
-		private:
-			BlockingQueue(const BlockingQueue& rhs);
-			BlockingQueue& operator= (const BlockingQueue& rhs);
+	public:
+		BlockingQueue() :mtx(), m_full(), m_empty(), m_capacity(MAX_CAPACITY) { }
+		void put(const T& task);
+		T take();
+		T front(); 
+		T back();
+		size_t size();
+		bool empty();
+		void setCapacity(const size_t capacity);
+	private:
+		BlockingQueue(const BlockingQueue& rhs);
+		BlockingQueue& operator= (const BlockingQueue& rhs);
 
-		private:
-			mutable std::mutex _mutex;
-			std::condition_variable _condvar;
-			std::list<T> _queue;
-	};
-
-	template class BlockingQueue<int>;
-	template class BlockingQueue<float>;
-	template class BlockingQueue<double>;
-
-}
+	private:
+		mutable std::mutex mtx;
+		std::condition_variable m_full;
+		std::condition_variable m_empty;
+		std::queue<T> m_queue;
+		size_t m_capacity; 
+};
 
 #endif
